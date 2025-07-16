3bb724d3bdb452340c66d09a5b4e5699820faabb
sleep-sleep-sheep <cld20051010@qq.com>
2025-07-16 19:54:14 +0800

    Update main.ml

--- toycc/bin/main.ml
+++ toycc/bin/main.ml
@@ -8,20 +8,12 @@ let parse_channel channel =
       let pos = lexbuf.lex_curr_p in
       failwith (Printf.sprintf "Syntax error at line %d, column %d"
                   pos.pos_lnum (pos.pos_cnum - pos.pos_bol))
-
 let () =
   try
-    print_endline "Enter code (Ctrl+D to finish):";
     let ast = parse_channel stdin in
-    print_endline "Parsing successful!";
-    
     Semantic.check_program ast;
-    print_endline "Semantic check passed!";
-    
     let ast_opt = Optimize.optimize_program ast in
     let riscv_code = Codegen.emit_program ast_opt in
-    
-    print_endline "Generated RISC-V code:";
     print_string riscv_code;
     flush stdout;
   with
