8a22003f2673eb8624acc515da5994a854dd6f68
sleep-sleep-sheep <cld20051010@qq.com>
2025-08-19 20:11:52 +0800

    Update optimize.ml

--- toycc/bin/optimize.ml
+++ toycc/bin/optimize.ml
@@ -464,11 +464,10 @@ let optimize program =
       let optimized = 
         program 
         |> fold_constants
-     (*   |> propagate_constants  *)
+        |> propagate_constants
         |> eliminate_dead_code
       in
       if optimized = program then optimized
       else optimize_iter optimized (iter + 1)
   in
   optimize_iter program 0
-
